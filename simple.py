a99c47e3455e8183c1cde80571f91058288ad232
WOUOGANG NZESSEU Nelson <nelson.wouogang@ensta-bretagne.org>
2020-04-25 14:53:20 +0200

    initial commit

--- simple.py
+++ simple.py
@@ -1,23 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  2 14:30:28 2020
 
-@author: nelson
-"""
+#Ce code fait la somme de deux nombres
 
-def ajoute_un(x):
-	return x+1
+def add(x,y):
+	return x+y
 
-#num1 = int(input("Quel est votre premier nombre? : "))
-#num2 = int(input("Quel est votre second nombre? : "))
-
-#s = add(num1,num2)
-#print("La somme de vos nombres donne %d " %s)
-
-#s = add(5,6)
-#somme_plus = ajoute_un(s)
-
-#print("La somme augmenter de un de vos nombres donne %d " %somme_plus)
 
 
